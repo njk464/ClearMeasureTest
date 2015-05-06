5eeb3a7349a0fa2974c8b09a0c7ea6056d207734
Nick Kantor <nick.kantor@utexas.edu>
2015-05-05 21:53:45 -0500

    Update ClearMeasureTest.cs

--- ClearMeasureTest.cs
+++ ClearMeasureTest.cs
@@ -1,19 +1,42 @@
-using System.IO;
+using System.Diagnostics;
 using System;
 
 class Program
 {
     static void Main()
     {
-        for (ulong i = 1; i < 10; i++)
-		{
-		    if ( i % 3 == 0 && i % 5 == 0)
-		        Console.WriteLine(i + ": BIZZ BUZZ");
-			if ( i % 3 == 0 )
-				Console.WriteLine(i + ": BIZZ");
-			if (i % 5 == 0) 
-				Console.WriteLine(i + ": BUZZ");
-			//Console.Write("\n");
-		}
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        ulong three = 1;
+        ulong five = 1;
+        string s = "";
+        while (three <= 333333)
+        {
+            ulong top = 333333;
+            if (three + 500 < 333333)
+                top = three + 500;
+            s = "";
+            while (three < top){
+                if (three*3 == five*5) {
+                    //Console.WriteLine(three*3 + ": BIZZ BUZZ");
+                    s += three * 3 + ": BIZZ BUZZ\n";
+                    three++;
+                    five++;
+                    continue;
+                }
+                if (five*5 < three*3) {
+                    //Console.WriteLine(five*5 + ": BUZZ");
+                    s += five * 5 + ": BUZZ\n";
+                    five++;
+                }
+                //Console.WriteLine(three*3 + ": BIZZ");
+                s += three * 3 + ": BIZZ\n";
+                three++;
+            }
+            Console.Write(s);
+	    }
+	    stopwatch.Stop();
+	    Console.WriteLine("Time elapsed: {0}",
+	    stopwatch.Elapsed);
     }
 }
