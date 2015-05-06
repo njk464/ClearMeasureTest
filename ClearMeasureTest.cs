805d193df0e303f9d1af034f85f0ef4cedee45bc
Nick Kantor <nick.kantor@utexas.edu>
2015-05-06 17:34:03 -0500

    Update ClearMeasureTest.cs

--- ClearMeasureTest.cs
+++ ClearMeasureTest.cs
@@ -1,42 +1,68 @@
-using System.Diagnostics;
+//using System.Diagnostics;
 using System;
 
 class Program
 {
     static void Main()
     {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-        ulong three = 1;
-        ulong five = 1;
-        string s = "";
-        while (three <= 333333)
+        //Stopwatch stopwatch = new Stopwatch();
+        //stopwatch.Start();
+        ulong size = 100000;
+        ulong i = 3;
+        string s2 = "";
+        // loop runs in a time of O(n/15) which is slightly better 
+        // than O(n).
+        // since every 15 numbers just repeats a pattern I was able
+        // to utilize that to make the code a little faster.
+        //  3,  5,  6,  9, 10, 12, 15
+        // 18, 20, 21, 24, 25, 27, 30
+        while (i <= size - 15)
         {
-            ulong top = 333333;
-            if (three + 500 < 333333)
-                top = three + 500;
-            s = "";
-            while (three < top){
-                if (three*3 == five*5) {
-                    //Console.WriteLine(three*3 + ": BIZZ BUZZ");
-                    s += three * 3 + ": BIZZ BUZZ\n";
-                    three++;
-                    five++;
-                    continue;
-                }
-                if (five*5 < three*3) {
-                    //Console.WriteLine(five*5 + ": BUZZ");
-                    s += five * 5 + ": BUZZ\n";
-                    five++;
-                }
-                //Console.WriteLine(three*3 + ": BIZZ");
-                s += three * 3 + ": BIZZ\n";
-                three++;
+            s2 += i + ": BIZZ\n";
+            i += 2;
+            s2 += i + ": BUZZ\n";
+            i++;
+            s2 += i + ": BIZZ\n";
+            i += 3;
+            s2 += i + ": BIZZ\n";
+            i++;
+            s2 += i + ": BUZZ\n";
+            i += 2;
+            s2 += i + ": BIZZ\n";
+            i += 3;
+            s2 += i + ": BIZZ BUZZ\n";
+            i += 3;
+            Console.Write(s2);
+            s2 = "";
+        }
+        // since the loop was broken up some of the final computation 
+        // has to be done at the end.
+        while (i <= size)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                s2 += i + ": BIZZ BUZZ\n";
+                i += 3;
+                continue;
             }
-            Console.Write(s);
-	    }
-	    stopwatch.Stop();
-	    Console.WriteLine("Time elapsed: {0}",
-	    stopwatch.Elapsed);
+            if (i % 3 == 0)
+            {
+                s2 += i + ": BIZZ\n";
+                i++;
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                s2 += i + ": BUZZ\n";
+                i++;
+                continue;
+            }
+            i++;
+        }
+        Console.Write(s2);
+        //stopwatch.Stop();
+        //Console.WriteLine("Time elapsed: {0}",
+        //stopwatch.Elapsed);
+
     }
 }
